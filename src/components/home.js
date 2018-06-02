53f824e69bbec7206a8a267ebf9dfc1a7cbb7538
Young <hogusong1@gmail.com>
2018-06-02 12:51:18 -0700

    complete browser for the begining mode

--- src/components/home.js
+++ src/components/home.js
@@ -3,8 +3,27 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div className="main-body">
-        Home
+      <div className="main-game-body">
+        <div className="puzzle-memo">
+          <h2>Some Memo</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut quos beatae magni magnam veniam illum quae perspiciatis? Quae ex, saepe obcaecati consectetur numquam sit ipsam ut aliquam illo et at.</p>
+        </div>
+        <div className="puzzle-main">
+          <div className="puzzle-game">
+            <div className="game-detail">
+              <p>Game Detail : </p>
+            </div>
+            <div className="puzzle-grid">
+              <p> Something</p>
+            </div>
+            <div className="puzzle-btns">
+              <h3>Some Buttons here</h3>
+            </div>
+          </div>
+          <div className="puzzle-options">
+            <h3>Some Radio Buttons here</h3>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/puzzle.js
+++ src/components/puzzle.js
@@ -21,7 +21,8 @@ class Puzzle extends Component {
     for (let i=0; i<9; i++) {
       for (let j=0; j<9; j++){
         const input = <input id={`${i}x${j}`} type='text'  
-          style={{background:`${this.background(i,j)}`}}/>
+          style={{background:`${this.background(i,j)}`}}
+          value={null}/>
         grids.push(input);
       }
     }
@@ -29,9 +30,37 @@ class Puzzle extends Component {
   }
   render() {
     return (
-      <div className="main-body">
-        <div className="puzzle-grid">
-          {this.renderGrid()}
+      <div className="main-game-body">
+        <div className="puzzle-memo">
+          <h2>Some Memo</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut quos beatae magni magnam veniam illum quae perspiciatis? Quae ex, saepe obcaecati consectetur numquam sit ipsam ut aliquam illo et at.</p>
+        </div>
+        <div className="puzzle-main">
+          <div className="puzzle-game">
+            <div className="game-detail">
+              <h3>Game Detail</h3>
+              <div id='detail'>
+                <p>Level : ..........</p>
+                <p>Puzzle #: .......</p>
+                <p>Hint count : ...</p>
+              </div>
+            </div>
+            <div className="puzzle-grid">
+              {this.renderGrid()}
+            </div>
+            <div className="puzzle-options">
+              <a>Press to get HELP</a>
+            </div>
+          </div>
+          <div className="puzzle-btns">
+            <button id='new'>New Game</button>
+            <button id='check'>Check Status</button>
+            <button id='hint'>One Hint</button>
+            <button id='clear'>Clear Wrong</button>
+            <button id='solve'>Solution</button>
+            <button id='restart'>Start Again</button>
+            <button id='addnew'>Add a Puzzle</button>
+          </div>
         </div>
       </div>
     );
